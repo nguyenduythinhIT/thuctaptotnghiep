637bb7a83c8951576e251daf3aaf85cfda261e9c
YunoVN <nguyenduythinh.it@outlook.com>
2018-04-07 15:02:35 +0700

    Initial commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,6 @@
+<html>
+        <head>
+                <title><?php echo $title; ?></title>
+                <link rel="stylesheet" type="text/css" href="<? echo base_url();?>/css/all.css">
+        </head>
+        <body>
